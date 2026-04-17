bd8f7d72f26d22e9c483293a2f5f6db3db2e2175
itsSkanda <itsabishekskanda@gmail.com>
2026-04-17 10:10:14 +0530

    Update Train_Consist_Management_App.java

--- src/Train_Consist_Management_App.java
+++ src/Train_Consist_Management_App.java
@@ -1,15 +1,27 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Train_Consist_Management_App {
-
+public class UC11 {
     public static void main(String[] args) {
 
-        System.out.println("=== Train Consist Management App ===");
+        System.out.println("=== Train Consist Management - UC11 ===");
 
-        List<String> trainConsist = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Train consist initialized.");
-        System.out.println("Initial number of bogies: " + trainConsist.size());
+        System.out.print("Enter Train ID: ");
+        String trainId = sc.nextLine();
+
+        System.out.print("Enter Cargo Code: ");
+        String cargoCode = sc.nextLine();
+
+        Pattern trainPattern = Pattern.compile("TRN-\\d{4}");
+        Pattern cargoPattern = Pattern.compile("PET-[A-Z]{2}");
+
+        Matcher trainMatcher = trainPattern.matcher(trainId);
+        Matcher cargoMatcher = cargoPattern.matcher(cargoCode);
+
+        System.out.println("Train ID Valid: " + trainMatcher.matches());
+        System.out.println("Cargo Code Valid: " + cargoMatcher.matches());
     }
 }
