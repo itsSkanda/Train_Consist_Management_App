8ca847a91eec68d48ac56f19cefb9505a10655ed
itsSkanda <itsabishekskanda@gmail.com>
2026-04-17 09:55:56 +0530

    Update Train_Consist_Management_App.java

--- src/Train_Consist_Management_App.java
+++ src/Train_Consist_Management_App.java
@@ -1,15 +1,25 @@
 import java.util.ArrayList;
-import java.util.List;
 
-public class Train_Consist_Management_App {
-
+public class UC2 {
     public static void main(String[] args) {
 
-        System.out.println("=== Train Consist Management App ===");
+        System.out.println("=== Train Consist Management - UC2 ===");
 
-        List<String> trainConsist = new ArrayList<>();
+        ArrayList<String> passengerBogies = new ArrayList<>();
 
-        System.out.println("Train consist initialized.");
-        System.out.println("Initial number of bogies: " + trainConsist.size());
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        System.out.println(passengerBogies);
+
+        passengerBogies.remove("AC Chair");
+
+        System.out.println(passengerBogies);
+
+        boolean exists = passengerBogies.contains("Sleeper");
+        System.out.println(exists);
+
+        System.out.println(passengerBogies);
     }
 }
